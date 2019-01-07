4699dc660907b479adbdc9c3da8987a3cda65a74
Nguyen Van Tuyen (FGA.FTEST) <TuyenNV22@fsoft.com.vn>
2019-01-07 17:49:39 +0700

    excel template and directory template class

---
+++ src/test/java/provider/DirectoryTest.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provider;
+
+import java.io.File;
+import java.net.URISyntaxException;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ *
+ * @author vantuyen361
+ */
+@RunWith(SpringRunner.class)
+public class DirectoryTest {
+
+    private Directory d;
+
+    @Before
+    public void before() {
+        d = new Directory();
+    }
+
+    @Test
+    public void testSomeMethod() {
+        String path = d.convertPathFromProjectFolder("guide\\readme.txt");
+        Assert.assertTrue("convert path failure, path:" + path, path.contains("skelet_selenium\\guide\\readme.txt"));
+    }
+
+    @Test
+    public void testFileFromClassPath() throws URISyntaxException {
+        try {
+            File file = d.fileFromClassPath("/application.properties");
+        } catch (NullPointerException e) {
+            Assert.assertTrue("not found file.", false);
+        }
+    }
+
+    @Test
+    public void testPathOfCurrentProject() throws URISyntaxException {
+        String path = d.pathOfCurrentProject();
+        Assert.assertTrue("don't get path of current project.", path.contains("skelet_selenium"));
+    }
+
+    @Test
+    public void testShowAllSystemProperties(){
+        d.showAllSystemProperties();
+    }
+}
